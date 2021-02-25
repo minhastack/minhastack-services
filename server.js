f7a9c8fd61121456d063f7fbfc030c908eed8619
Gabriel Vanderlei <gabrielvanderlei10@hotmail.com>
2021-02-24 22:15:56 -0300

    add env port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 
 const app = express()
-const port = 21624
+const port = process.env.PORT || 21624
 
 app.use(express.json());
 app.use(cors());
